a1fdd3d23bde40c05860b18b9beb047ca8bbb065
ngumina17 <nicole.gumina17@gmail.com>
2020-01-06 10:04:22 -0600

    data renders in console

--- recipe/src/App.js
+++ recipe/src/App.js
@@ -1,26 +1,52 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+ class App extends Component {
+   constructor(props) {
+     super(props)
+     this.state={
+       meals: []
+     };
+   }
+ handleClick = (e) => {
+   console.log('componentDidMount')
+   fetch ('https://www.themealdb.com/api/json/v1/1/search.php?s')
+   .then(res => res.json())
+   .then(newRecipe => {console.log('app - newRecipe' , newRecipe)
+   let strMeal = newRecipe.strMeal
+   let strInstructions = newRecipe.strInstructions
+   this.setState({strMeal})
+   this.setState({strInstructions})
+  })
+ }
 
-export default App;
+ componentDidMount () {
+   this.handleClick()
+ }
+
+ render () {
+   return (
+     <div className="App">
+       <header className='App-header'>
+         <h1 className='Recipe-Name'>Recipes</h1>
+       </header>
+       <div>
+         <p>{this.state.meals}</p>
+         <p>{this.state.strMeal}</p>
+         <p>{this.state.strInstructions}</p>
+       </div>
+       <button onClick={this.handleClick}>New Recipe</button>
+     </div>
+   )
+ }
+
+
+
+ }
+
+ export default App;
+
+
+
+
