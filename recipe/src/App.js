4b68b5f5afa62db7c8f6ce76e48e73e1a674b8de
ngumina17 <nicole.gumina17@gmail.com>
2020-01-07 13:36:23 -0600

    project overview

--- recipe/src/App.js
+++ recipe/src/App.js
@@ -1,68 +1,71 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-// import { Route, Link } from "react-router-dom";
+//  import { Route, Link } from "react-router-dom";
 import Home from './Home';
 
- class App extends Component {
-   constructor(props) {
-     super(props)
-     this.state={
-       meals: [],
+const url = 'https:www.themealdb.com/api/json/v1/1/search.php?s'
+
+class App extends Component {
+
+    constructor() {
+      super()
+      this.state={
+        meals: [],
   
-     };
-   }
- handleClick = (e) => {
-   console.log('componentDidMount')
-   fetch ('https://www.themealdb.com/api/json/v1/1/search.php?s')
-   .then(res => res.json())
-   .then(newRecipe => {
-      // let strMeal = newRecipe.meals[0].strMeal
-      // let strInstructions = newRecipe.meals[0].strInstructions
-      // let strIngredient = newRecipe.meals[0].strIngredient
-      let meal = this.state.meals
-      let mealsArr = Object.keys(meal)
-      let ingredientsArr = []
-      for (let i=0; i< mealsArr.length; i++) {
-        const key = mealsArr[i]
-        const value = meal[key]
-        console.log("value" + value)
-        if (value === null) {
-          //do nothing
-        } else if (key.includes('strIngredient')) {
-          ingredientsArr.push(value)
-        }
-      }
-      // this.setState({strMeal})
-      // this.setState({strInstructions})
-      // this.setState({strIngredient})
+      };
+    }
+  handleClick = (e) => {
+    console.log('componentDidMount')
+    fetch ('https:www.themealdb.com/api/json/v1/1/search.php?s')
+    .then(res => res.json())
+    .then(newRecipe => {
+        let strMeal = newRecipe.meals[0].strMeal
+        let strInstructions = newRecipe.meals[0].strInstructions
+        let strIngredient = newRecipe.meals[0].strIngredient
+      //  let meal = this.state.meals
+      //  let mealsArr = Object.keys(meal)
+      //  let ingredientsArr = []
+      //  for (let i=0; i< mealsArr.length; i++) {
+      //    const key = mealsArr[i]
+      //    const value = meal[key]
+      //    console.log("value" + value)
+      //    if (value === null) {
+      //      do nothing
+      //    } else if (key.includes('strIngredient')) {
+      //      ingredientsArr.push(value)
+      //    }
+      //  }
+        this.setState({strMeal})
+        this.setState({strInstructions})
+        this.setState({strIngredient})
       
-      })
+       })
   
  
- }
+  }
 
- componentDidMount () {
-   this.handleClick()
- }
+  componentDidMount () {
+    this.handleClick()
+  }
 
- render () {
-   return (
-     <div className="App">
-       <header className='App-header'>
-         <h1 className='Recipe-Name'>Recipes</h1>
-       </header>
-       <div>
-         <p>{this.state.meal}</p>
-         <p>{this.state.strMeal}</p>
-         <p>{this.state.strInstructions}</p>
-         <p>{this.state.ingredientsArr}</p>
+  render () {
+    return (
+      <div className="App">
+        <header className='App-header'>
+          <h1 className='Recipe-Name'>Recipes</h1>
+        </header>
+        <div>
+          <p>{this.state.meal}</p>
+          <p>{this.state.strMeal}</p>
+          <p>{this.state.strInstructions}</p>
+          <p>{this.state.ingredientsArr}</p>
 
-       </div>
-       <button onClick={this.handleClick}>New Recipe</button>
-     </div>
-   )
- }
+        </div>
+        <button onClick={this.handleClick}>New Recipe</button>
+      </div>
+    )
+  }
 
 
 
